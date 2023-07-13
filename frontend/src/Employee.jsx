bd32cc71a2dc07ca406ae27c0a13f9dfbe85aae7
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-13 14:10:09 +0300

    creating employee page

--- frontend/src/Employee.jsx
+++ frontend/src/Employee.jsx
@@ -1,5 +1,32 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function Employee() {
-  return <div>Employee</div>;
+  return (
+    <div className="px-5 py-3">
+      <div className="d-flex justify-content-center mt-2">
+        <h3>Employee List</h3>
+      </div>
+      <Link to="/create" className="btn btn-success">
+        Add Employee
+      </Link>
+      <div className="mt-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Salary</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Employee;
