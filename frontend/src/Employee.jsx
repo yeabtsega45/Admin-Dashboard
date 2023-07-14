4c3fcc354f9e0e78037ab41aa95ec1c2fe37ccb5
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-14 12:11:50 +0300

    displaying employees on the database

--- frontend/src/Employee.jsx
+++ frontend/src/Employee.jsx
@@ -1,8 +1,36 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Employee() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/getEmployee")
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          setData(res.data.Result);
+        } else {
+          alert("Error");
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:8081/delete/" + id)
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          window.location.reload(true);
+        } else {
+          alert("Error");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="px-5 py-3">
       <div className="d-flex justify-content-center mt-2">
@@ -23,6 +51,41 @@ function Employee() {
               <th>Action</th>
             </tr>
           </thead>
+          <tbody>
+            {data.map((employee, index) => {
+              return (
+                <tr key={index}>
+                  <td>{employee.name}</td>
+                  <td>
+                    {
+                      <img
+                        src={`http://localhost:8081/images/` + employee.image}
+                        alt=""
+                        className="employee_image"
+                      />
+                    }
+                  </td>
+                  <td>{employee.email}</td>
+                  <td>{employee.address}</td>
+                  <td>{employee.salary}</td>
+                  <td>
+                    <Link
+                      to={`/employeeEdit/` + employee.id}
+                      className="btn btn-primary btn-sm me-2"
+                    >
+                      edit
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(employee.id)}
+                      className="btn btn-sm btn-danger"
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
