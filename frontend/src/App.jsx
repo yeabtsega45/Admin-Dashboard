470156911ed54e57bbde91c32609e0f2cba0b3b4
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-16 22:26:18 +0300

    separating login for admin and users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Employee from "./Employee";
 import Profile from "./Profile";
 import Home from "./Home";
 import AddEmployee from "./AddEmployee";
 import EditEmployee from "./EditEmployee";
+import Start from "./Start";
+import EmployeeDetail from "./EmployeeDetail";
+import EmployeeLogin from "./EmployeeLogin";
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
           <Route path="/employeeEdit/:id" element={<EditEmployee />}></Route>
         </Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/start" element={<Start />}></Route>
+        <Route path="/employeeLogin" element={<EmployeeLogin />}></Route>
+        <Route path="/employeedetail/:id" element={<EmployeeDetail />}></Route>
       </Routes>
     </BrowserRouter>
   );
